dfbdb370f566d2e74cf97914ec3eaf3aa70adbc6
Faithful1 <faithfulanere@gmail.com>
2024-02-03 13:07:43 +0100

    Add metadata to blog page and update layout metadata

--- chores/src/app/blog/page.tsx
+++ chores/src/app/blog/page.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title:{
+        absolute: "chores",
+
+    },
+    description: "My blog",
+};
+
 export default function blog() {
     return (
         <div>
