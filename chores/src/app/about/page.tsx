172291ca9b7a146708b7490b9764a34fe3cc2551
Faithful1 <faithfulanere@gmail.com>
2024-02-03 12:53:48 +0100

    Add metadata and generateMetadata function for About and ProductDetails pages

--- chores/src/app/about/page.tsx
+++ chores/src/app/about/page.tsx
@@ -1,3 +1,9 @@
+export const metadata = {
+    title: "About",
+    description: "About by Chores",
+};
+
+
 export default function About() {
     return (
         <div>
--- chores/src/app/products/[productId]/page.tsx
+++ chores/src/app/products/[productId]/page.tsx
@@ -1,8 +1,24 @@
-export default function ProductDetails({ params }: {
+import { Metadata } from "next";
+
+type Props = {
     params: {
         productId: string
     }
-}) {
+}
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata>  =>  {
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Product ${params.productId}`);
+        }, 1000);
+    });
+    return {
+        title: `Product ${params.productId}`,
+        description: `Details about product ${params.productId}`
+    }
+}
+
+export default function ProductDetails({ params }: Props) {
     return (
         <div>
             <h1>Details about product { params.productId }</h1>
