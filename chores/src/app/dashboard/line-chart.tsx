9707a986d42f5fde3e697ab391748337cf2c2510
Faithful1 <faithfulanere@gmail.com>
2024-02-03 11:03:29 +0100

    colocation

---
+++ chores/src/app/dashboard/line-chart.tsx
@@ -0,0 +1,8 @@
+export default function LineChart() {
+    return (
+        <div>
+            <h1>Line chart</h1>
+            <h2>View charts</h2>
+        </div>
+    );
+}
---
+++ chores/src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard things like { barChart() }</h1>
+        </div>
+    );
+}
+
+function barChart() {
+    return (
+        <div>
+            <h1>Barchart</h1>
+        </div>
+    );
+}
