72b8e4c844a6074cc3bbc96357d23ec232f2300c
Faithful1 <faithfulanere@gmail.com>
2024-02-03 09:30:24 +0100

    nested routing

---
+++ chores/src/app/blog/first/page.tsx
@@ -0,0 +1,7 @@
+export default function firstBlog() {
+    return (
+        <div>
+            <h1>My first blog post</h1>
+        </div>
+    );
+}
---
+++ chores/src/app/blog/second/page.tsx
@@ -0,0 +1,7 @@
+export default function secondBlog() {
+    return (
+        <div>
+            <h1>My second blog post</h1>
+        </div>
+    );
+}
